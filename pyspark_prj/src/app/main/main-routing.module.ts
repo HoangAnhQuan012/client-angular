07d0dd0ff0d2874cb373e12214e7bfedcd7171a7
quanha <quanha@dft.vn>
2024-01-07 23:15:12 +0700

    Tích hợp primeng và demo component

--- pyspark_prj/src/app/main/main-routing.module.ts
+++ pyspark_prj/src/app/main/main-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DemoComponent } from './demo/demo.component';
 
 const routes: Routes = [
-  { path: '', children: [] }
-];
+  { path: '', children: [
+    {
+      path: 'demo', component: DemoComponent
+    }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
